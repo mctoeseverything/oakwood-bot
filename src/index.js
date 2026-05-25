e624d459f1c1780d5b9a4f3209478024cb05fedf
mctoeseverything <mctoeseverything@gmail.com>
2026-05-25 15:46:02 -0500

    etjy,fhmtymd

--- src/index.js
+++ src/index.js
@@ -2,29 +2,6 @@ require('dotenv').config();
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const { loadCommands } = require('./handlers/commandHandler');
 const { loadEvents } = require('./handlers/eventHandler');
-
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.MessageContent,
-  ],
-});
-
-client.commands = new Collection();
-client.cooldowns = new Collection();
-
-(async () => {
-  await loadCommands(client);
-  await loadEvents(client);
-  await client.login(process.env.BOT_TOKEN);
-})();
-
-require('dotenv').config();
-const { Client, GatewayIntentBits, Collection } = require('discord.js');
-const { loadCommands } = require('./handlers/commandHandler');
-const { loadEvents } = require('./handlers/eventHandler');
 const http = require('http');
 
 const client = new Client({
