b33e19d90a87d666ca522a0d27acb0051fd5ba89
mctoeseverything <mctoeseverything@gmail.com>
2026-05-25 15:44:53 -0500

    wetjmwjwjwjhrw67

--- src/index.js
+++ src/index.js
@@ -20,3 +20,34 @@ client.cooldowns = new Collection();
   await loadEvents(client);
   await client.login(process.env.BOT_TOKEN);
 })();
+
+require('dotenv').config();
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+const { loadCommands } = require('./handlers/commandHandler');
+const { loadEvents } = require('./handlers/eventHandler');
+const http = require('http');
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.commands = new Collection();
+client.cooldowns = new Collection();
+
+// Keep-alive HTTP server for Render
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('OK');
+});
+server.listen(process.env.PORT || 3000);
+
+(async () => {
+  await loadCommands(client);
+  await loadEvents(client);
+  await client.login(process.env.BOT_TOKEN);
+})();
